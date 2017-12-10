0f81bf3d06ae37bae023720a8a9e589769a97cd5
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2017-12-11 00:10:43 +0100

    vuejs + itcss norm in place

---
+++ src/components/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+
+    <div class="test">
+    {{ msg }}
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vuejs App'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+   // @import 'welcome.scss'
+</style>
