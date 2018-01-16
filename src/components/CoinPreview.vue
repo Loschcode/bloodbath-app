aa68dc724c4c8b0e9d2314faaf02b315872d558e
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-16 19:54:12 +0100

    updated data on homepage (but failing at the same time)

--- src/components/CoinPreview.vue
+++ src/components/CoinPreview.vue
@@ -8,7 +8,25 @@
           <h2>{{ marketCoin.name }}</h2>
         </div>
         <div class="module__content">
-          {{ marketCoin.price }}
+
+          <div class="module__content-digits">
+            <div v-if="marketCoin.price">
+              <animated-number :value="marketCoin.price" :type="`money`" />
+            </div>
+            <div v-else>
+              -
+            </div>
+          </div>
+
+          <div class="module__content-percent --small">
+            <div v-if="rawVariation()">
+              <animated-number :value="rawVariation()" :type="`percent`" :animatedColors="false" :numberColors="true" />
+            </div>
+            <div v-else>
+              -
+            </div>
+          </div>
+
         </div>
         <div class="module__footer">
         </div>
@@ -18,13 +36,36 @@
   </div>
 </template>
 <script>
+import AnimatedNumber from '@/components/AnimatedNumber'
+
 export default {
   props: [
     'marketCoin'
   ],
 
+  created () {
+    this.$drycable.subscribe(this, 'marketCoin')
+  },
+
   data () {
-    return {}
+    return {
+    }
+  },
+
+  methods: {
+    // this is a copy paste (for now) from Coin.vue
+    rawVariation () {
+      let digits = this.marketCoin.price / this.marketCoin.day_open - 1
+      if (isNaN(digits)) {
+        return 0.0
+      } else {
+        return digits
+      }
+    }
+  },
+
+  components: {
+    AnimatedNumber
   }
 }
 </script>
