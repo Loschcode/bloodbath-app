e84655ea1783c9741f8356488b20ac625e14db28
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-20 13:50:30 +0100

    progress on footer styling, change header

--- src/components/DefaultFooter.vue
+++ src/components/DefaultFooter.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="row row-full footer">
       <div class="gr-4">
+        <div class="footer__title">Coins</div>
+        <div class="footer__content">$10,000 BTC</div>
       </div>
       <div class="gr-4">
+        <div class="footer__title">Portfolio</div>
+        <div class="footer__content">$10,000</div>
       </div>
       <div class="gr-4">
+        <div class="footer__title">My account</div>
+        <div class="footer__content">You are not logged-in</div>
       </div>
     </div>
   </div>
