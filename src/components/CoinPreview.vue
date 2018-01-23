5e062721d71ffcab3506c9cb1c544f09d6c87afd
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-01-23 13:34:57 +0000

    refactoring modules coins

--- src/components/CoinPreview.vue
+++ src/components/CoinPreview.vue
@@ -2,40 +2,14 @@
   <div class="coin-preview">
     <div v-if="marketCoin">
       <router-link :to="{ name: 'coin', params: { coinName: marketCoin.symbol } }">
-
-        <!-- Base Price -->
+        
         <div class="gr-12 gr-12@mobile">
           <div class="module">
             <div class="module__title">
-              <div class="row">
-                <div class="gr-2">
-                  <coin-preview-favorite :userMarketCoinProp="userMarketCoin" />
-                </div>
-                <div class="gr-10">
-                  <h2>{{ marketCoin.coin_name }} <span class="module__subtitle">{{ marketCoin.name }}</span></h2>
-                </div>
-              </div>
+              <coin-preview-title :userMarketCoinProp="userMarketCoin" :marketCoinProp="marketCoin" />
             </div>
             <div class="module__content">
-
-              <div class="module__content-digits --medium">
-                <div v-if="marketCoin.price">
-                  <animated-number :value="marketCoin.price" :type="`money`" />
-                </div>
-                <div v-else>
-                  -
-                </div>
-              </div>
-
-              <div class="module__content-percent --small">
-                <div v-if="rawVariation()">
-                  <animated-number :value="rawVariation()" :type="`percent`" :animatedColors="false" :numberColors="true" />
-                </div>
-                <div v-else>
-                  -
-                </div>
-              </div>
-
+              <coin-preview-content :userMarketCoinProp="userMarketCoin" :marketCoinProp="marketCoin" />
             </div>
             <div class="module__footer">
             </div>
@@ -48,9 +22,8 @@
 </template>
 
 <script>
-import AnimatedNumber from '@/components/AnimatedNumber'
-import CoinPreviewFavorite from '@/components/CoinPreviewFavorite'
-import ThrowError from '@/mixins/ThrowError'
+import CoinPreviewTitle from '@/components/CoinPreviewTitle'
+import CoinPreviewContent from '@/components/CoinPreviewContent'
 
 export default {
   props: [
@@ -87,24 +60,11 @@ export default {
   },
 
   methods: {
-    // this is a copy paste (for now) from Coin.vue
-    rawVariation () {
-      let digits = this.marketCoin.price / this.marketCoin.day_open - 1
-      if (isNaN(digits)) {
-        return 0.0
-      } else {
-        return digits
-      }
-    }
   },
 
   components: {
-    AnimatedNumber,
-    CoinPreviewFavorite
-  },
-
-  mixins: [
-    ThrowError
-  ]
+    CoinPreviewTitle,
+    CoinPreviewContent
+  }
 }
 </script>
---
+++ src/components/CoinPreviewContent.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="coin-preview-content">
+    <div class="module__content-digits --medium">
+      <div v-if="marketCoinProp.price">
+        <animated-number :value="marketCoinProp.price" :type="`money`" />
+      </div>
+      <div v-else>
+        -
+      </div>
+    </div>
+
+    <div class="module__content-percent --small">
+      <div v-if="rawVariation()">
+        <animated-number :value="rawVariation()" :type="`percent`" :animatedColors="false" :numberColors="true" />
+      </div>
+      <div v-else>
+        -
+      </div>
+    </div>
+
+  </div>
+</div>
+</template>
+
+<script>
+import AnimatedNumber from '@/components/AnimatedNumber'
+
+export default {
+  props: [
+    'marketCoinProp',
+    'userMarketCoinProp'
+  ],
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+    rawVariation () {
+      let digits = this.marketCoinProp.price / this.marketCoinProp.day_open - 1
+      if (isNaN(digits)) {
+        return 0.0
+      } else {
+        return digits
+      }
+    }
+  },
+
+  components: {
+    AnimatedNumber
+  }
+}
+</script>
