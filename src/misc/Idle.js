b1b7a883f569fdf93ca70553534080fd99f12ab7
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-02-15 17:12:05 +0100

    idle system improved

--- src/misc/Idle.js
+++ src/misc/Idle.js
@@ -4,14 +4,20 @@ var idle = {
   interval: 60000,
   timeout: 1, // 1 minute total
   idleCounter: 0,
+  expirationConfirmed: false,
 
   expired () {
     EventBus.$emit('rebootEvent')
   },
 
   checker () {
+    var self = this
     document.onmousemove = document.onkeypress = function () {
-      this.idleCounter = 0
+      if (self.expirationConfirmed) {
+        self.expired()
+      } else {
+        this.idleCounter = 0
+      }
     }
   },
 
@@ -19,7 +25,7 @@ var idle = {
     var self = this
     window.setInterval(function () {
       if (++self.idleCounter >= self.timeout) {
-        self.expired()
+        self.expirationConfirmed = true
       }
     }, this.interval)
   },
