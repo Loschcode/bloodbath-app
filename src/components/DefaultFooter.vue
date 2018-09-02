58b66e49ab912099d61a43ea55690f172a93eea3
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-09-02 23:04:34 +0200

    weather switch moved

--- src/components/DefaultFooter.vue
+++ src/components/DefaultFooter.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="default-footer">
     <div class="row">
-      <!-- For all suggestions -->
-      <div class="gr-12">
+      <div class="gr-8">
         <!-- Suggestion -->
         <div class="inner-footer">
           <a href="mailto:laurent.schaffner.code@gmail.com">
             <span class="icon-contact"></span> Any suggestion or bug report ? Please let me know
           </a>
+        </div>
+      </div>
+
+      <div class="footer__actions">
+        <div v-if="userSetting">
+          <slot name="right">
+            <div v-if="userSetting.weather">
+              <div @click="weatherOff" class="footer__actions--selected +pointer">
+                <span>Weather : On</span>
+              </div>
+            </div>
+            <div v-else>
+              <div @click="weatherOn" class="footer__actions--unselected +pointer">
+                <span>Weather : Off</span>
+              </div>
+            </div>
+          </slot>
         </div>
       </div>
       </div>
@@ -25,6 +41,13 @@ export default {
   },
 
   methods: {
+    weatherOn () {
+      this.$store.dispatch('updateUserSetting', { changeset: { weather: true } })
+    },
+
+    weatherOff () {
+      this.$store.dispatch('updateUserSetting', { changeset: { weather: false } })
+    }
   },
 
   created () {
@@ -34,6 +57,9 @@ export default {
   },
 
   computed: {
+    userSetting () {
+      return this.$store.getters.getUserSetting
+    }
   },
 
   components: {
