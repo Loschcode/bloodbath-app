312d993b916ceae407a2985c94365fe789692281
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-09-04 23:03:22 +0200

    weather merged with investment

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -6,18 +6,13 @@
       </div>
     </default-header>
 
-    <!-- Capital weather -->
-    <div v-if="userSetting.weather && portfolioCoins.length">
-      <portfolio-weather :variationProp="totalVariation" />
-    </div>
-
     <!-- My capital -->
     <div v-if="portfolioCoins.length">
       <div class="section">
         <div class="row">
           <div class="gr-12">
             <div class="section__title">
-              Current investment
+              Total investment
             </div>
           </div>
         </div>
@@ -30,6 +25,24 @@
                 <div class="module__bubble">
 
                   <div class="row">
+                    <div class="gr-6 gr-centered gr-12@mobile gr-6@tablet">
+                      <div class="market-weather__icon">
+                        <span :class="`icon-${currentStyle()} coin-weather__${currentStyle()} coin-weather--static`"></span>
+                      </div>
+
+                      <div class="market-weather__title">
+                        <span><coin-weather :variationProp="totalVariation" /></span>
+                      </div>
+
+                      <div class="market-weather__info">
+                        <span>based on your total capital</span>
+                      </div>
+
+                    </div>
+                  </div>
+
+                  <div class="row">
+
                     <div class="gr-4 gr-centered gr-12@mobile gr-6@tablet">
 
                       <div class="module__content-digits --extra-big">
@@ -56,6 +69,18 @@
                             </div>
                           </div>
                         </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="gr-10">
+                      <div class="module__footer-details">
+                      </div>
+                    </div>
+                    <div class="gr-2">
+                      <div class="module__footer-action +no-margin-top">
+                        <a><span @click="goPortfolioFullWeather" class="icon icon-fullscreen"></span></a>
                       </div>
                     </div>
                   </div>
@@ -105,15 +130,17 @@
 </template>
 
 <script>
+import CoinWeather from '@/components/CoinWeather'
+import Weather from '@/misc/Weather'
 import AnimatedNumber from '@/components/AnimatedNumber'
 import DefaultFooter from '@/components/DefaultFooter'
 import DefaultHeader from '@/components/DefaultHeader'
 import PortfolioCoin from '@/components/PortfolioCoin'
 import PortfolioCapital from '@/components/PortfolioCapital'
-import PortfolioWeather from '@/components/PortfolioWeather'
 import SearchCoins from '@/components/SearchCoins'
 import EventBus from '@/misc/EventBus'
-// import _ from 'lodash'
+
+import router from '@/router'
 
 import { mapGetters } from 'vuex'
 
@@ -146,15 +173,23 @@ export default {
   },
 
   methods: {
+    goPortfolioFullWeather () {
+      router.push({ name: 'portfolio-full-weather', params: { } })
+    },
+
+    currentStyle () {
+      return Weather.style(this.totalVariation)
+    }
   },
 
   components: {
     AnimatedNumber,
+    CoinWeather,
+    Weather,
     DefaultFooter,
     DefaultHeader,
     PortfolioCapital,
     PortfolioCoin,
-    PortfolioWeather,
     SearchCoins,
     EventBus
   }
